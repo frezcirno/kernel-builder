fbe60adfe0d1679c71b7521fb980bfd51fae5072
frezcirno <2277861660@qq.com>
2022-01-31 23:36:42 +0800

    init repo

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+BASE=$(realpath $(dirname $0))
+SRCDIR=$BASE/src
+PATCHDIR=$BASE/patches
+CONFIG=$BASE/.config
+
+if [ $# != 1 ]; then
+	echo "usage: $0 <kernel_version>"
+	echo "	eg: $0 5.16.4"
+	exit 1
+fi
+
+VERSION=$1
+SOURCE_URL=https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$VERSION.tar.xz
+SOURCE=$SRCDIR/linux-$VERSION
+
+if [ ! -d $SOURCE ]; then
+	wget -qO- $SOURCE_URL | tar -C $SRCDIR -Jxvf -
+fi
+
+for f in $PATCHDIR/*; do
+	patch -p1 -N -d $SOURCE < $f || true
+done
+
+sudo cp $CONFIG $SOURCE/.config
+
+docker run --rm -v $SRCDIR:/src kernel-builder:latest \
+	make V=1 -C /src/linux-$VERSION -j$(nproc) bindeb-pkg
