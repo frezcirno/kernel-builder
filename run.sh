2773cacd88d873e51d7950f2cdcdf4835536a2f8
frezcirno <2277861660@qq.com>
2022-02-07 15:56:04 +0800

    fix build errors

--- run.sh
+++ run.sh
@@ -5,27 +5,62 @@ BASE=$(realpath $(dirname $0))
 SRCDIR=$BASE/src
 PATCHDIR=$BASE/patches
 
-if [ $# != 1 ]; then
-	echo "usage: $0 <kernel_version>"
-	echo "	eg: $0 5.16.4"
-	exit 1
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -e|--extension)
+            EXTENSION="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -j)
+            NPROC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -d|--debug)
+            DEBUG=YES
+            shift # past argument
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+
+if [ ${#POSITIONAL_ARGS[@]} != 1 ]; then
+    echo "usage: $0 <kernel_version>"
+    echo "	eg: $0 5.16.4"
+    exit 1
 fi
 
-VERSION=$1
+VERSION=${POSITIONAL_ARGS[0]}
 SOURCE_URL=https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$VERSION.tar.xz
 SOURCE=$SRCDIR/linux-$VERSION
 
 if [ ! -d $SOURCE ]; then
-	wget -qO- $SOURCE_URL | tar -C $SRCDIR -Jxvf -
+    wget -qO- $SOURCE_URL | tar -C $SRCDIR -Jxvf -
 fi
 
 for f in $PATCHDIR/*; do
-	patch -p1 -N -d $SOURCE < $f || true
+    patch -p1 -N -d $SOURCE < $f || true
 done
 
 if [ ! -f $SOURCE/.config ]; then
-	cp $SRCDIR/.config $SOURCE/.config
+    cp $SRCDIR/.config $SOURCE/.config
 fi
 
-docker run --rm -v $SRCDIR:/src kernel-builder:latest \
-	make V=1 -C /src/linux-$VERSION -j$(nproc) deb-pkg
+rm $SOURCE/vmlinux-gdb.py || true
+rm -rf $SOURCE/debian || true
+
+if [ -z $DEBUG ]; then
+    docker run --rm -v $SRCDIR:/src kernel-builder:latest make -C /src/linux-$VERSION -j${NPROC:-$(nproc)} deb-pkg |& tee build.log
+else
+    docker run --rm -it -v $SRCDIR:/src kernel-builder:latest bash
+fi
